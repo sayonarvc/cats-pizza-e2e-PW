8f39151f646a1eb0c70273926eba65f64080435c
sayonarvc <vladimir.perminov.99@mail.ru>
2026-09-21 21:17:19 +0500

    add testt && add data-testid {index}

--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -1,9 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-test('Проверка открытия главной страницы', async ({ page }) => {
+test('Check header', async ({ page }) => {
   await page.goto('');
-
   const header = page.getByTestId('homePageHeader');
 
   await expect(header).toBeVisible();
+});
+
+test('Check card list', async ({ page }) => {
+  await page.goto('');
+  const firstCard = page.getByTestId('catCard_0');
+  const cardListItems = page.getByTestId(/catCard/);
+
+  await expect(firstCard).toBeVisible();
+  await expect(await cardListItems.count()).toBeGreaterThan(0);
+  await expect(await cardListItems.count()).toBe(9);
 });
