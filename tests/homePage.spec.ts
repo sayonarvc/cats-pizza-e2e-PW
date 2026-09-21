4b8d53ca2f30c4e39a9c8c5d6ffbc3c897405bf7
sayonarvc <vladimir.perminov.99@mail.ru>
2026-09-21 20:46:33 +0500

    add data-testid in front

--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -3,5 +3,7 @@ import { test, expect } from '@playwright/test';
 test('Проверка открытия главной страницы', async ({ page }) => {
   await page.goto('');
 
-  await expect(page.getByRole('heading', { name: 'Заказ котиков' })).toBeVisible();
+  const header = page.getByTestId('homePageHeader');
+
+  await expect(header).toBeVisible();
 });
